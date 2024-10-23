74603cb751294b176b67477f69cb14e8433b56a9
Subin Kumar <isubinkr@icloud.com>
2024-10-24 03:18:02 +0530

    implement channel statistics and video retrieval features

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,123 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  // const subscribersCount = await Subscription.countDocuments({ channel: req.user._id });
+  const subscribersStats = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalSubscribers: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  const videosStats = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+      },
+    },
+    {
+      $project: {
+        likesCount: {
+          $size: "$likes",
+        },
+        views: 1,
+        commentsCount: {
+          $size: "$comments",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalLikes: {
+          $sum: "$likesCount",
+        },
+        totalComments: {
+          $sum: "$commentsCount",
+        },
+        totalViews: {
+          $sum: "$views",
+        },
+        totalVideos: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  const channelStats = {
+    totalSubscribers: subscribersStats[0]?.totalSubscribers || 0,
+    totalVideos: videosStats[0]?.totalVideos || 0,
+    totalView: videosStats[0]?.totalViews || 0,
+    totalLikes: videosStats[0]?.totalLikes || 0,
+    totalComments: videosStats[0]?.totalComments || 0,
+  };
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, channelStats, "Channel stats fetched successfully")
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        isPublished: 1,
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Videos fetched successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
